d91b1f7a769310e604f8ef80b3a6acabb15e83a4
Alwin George <150264978+Alwin444@users.noreply.github.com>
2025-03-15 13:16:43 +0530

    Update Sort.asm

--- Sort.asm
+++ Sort.asm
@@ -1,46 +1,44 @@
 data segment
 n db 00h
-min db 00h
 ar db 00h
 data ends
 code ends
 assume cs:code;ds:data
-start:  mov ax,data
-		mov ds,ax
-		lea si,ar
-		lea di,ar
-		mov bh,n
-		dec bh
-		mov bl,0
-		mov dh,n
-		inc dh
-		ag: cmp bh,00h
-		    jz out
-		    mov ah,[si]
-			inc bl
-			mov di,si
-			mov dl,bl
-			mov CX,si
-			ig1: cmp dl,dh
-				 jz sk
-				 cmp [di],ah
-				 jnc sk1
-				 mov CX,di
-				 mov al,[di]
-				 mov ah,al
-				sk1: inc dl
-				     inc di
-				 	 jmp ig1
-			sk:	cmp si,CX
-				jz fin
-				mov di,CX
-				mov al,[di]
-				mov ah,[si]
-				mov [di],ah
-				mov [si],al
-		    fin: inc si
-				 dec bh
-			     jmp ag
-		out: int 3
-		code ends
-		end start
+start: mov ax,data
+       mov ds,ax
+       lea si,ar
+       lea di,ar
+       mov bh,n
+       dec bh
+       mov bl,0
+       mov dh,n
+       inc dh
+       ag: cmp bh,00h
+	   jz out
+	   mov ah,[si]
+	   inc bl
+	   mov di,si
+	   mov dl,bl
+	   mov CX,si
+           ig1: cmp dl,dh
+		jz sk
+		cmp [di],ah
+		jnc sk1
+		mov CX,di
+		mov ah,[di]
+		sk1: inc dl
+		     inc di
+		     jmp ig1
+	   sk: cmp si,CX
+	       jz fin
+	       mov di,CX
+	       mov al,[di]
+	       mov ah,[si]
+	       mov [di],ah
+	       mov [si],al
+	       fin: inc si
+		    dec bh
+		    jmp ag
+	out: int 3
+	code ends
+	end start
